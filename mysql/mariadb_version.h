94f7b51e7ce073d4d11e60bc335fd85377b460e5
Karen Arutyunov <karen@codesynthesis.com>
2019-01-16 21:51:26 +0300

    Update copyright year

--- mysql/mariadb_version.h
+++ mysql/mariadb_version.h
@@ -1,6 +1,6 @@
 /*
  * file      : mysql/mariadb_version.h
- * copyright : Copyright (c) 2009-2018 Code Synthesis Tools CC
+ * copyright : Copyright (c) 2009-2019 Code Synthesis Tools CC
  * license   : LGPLv2.1; see accompanying COPYING file
  */
 
